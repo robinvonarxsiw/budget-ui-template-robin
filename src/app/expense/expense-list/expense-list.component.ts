c4eeff5e6949b85d3ca1448c964f32d2cbdb7919
robinvonarxsiw <robin.vonarx@lernen.siw.swiss>
2024-03-24 11:35:13 +0100

    Method "Load Expenses" implemented and HTML Adjustment with ion-item-group started

--- src/app/expense/expense-list/expense-list.component.ts
+++ src/app/expense/expense-list/expense-list.component.ts
@@ -2,7 +2,18 @@ import { Component } from '@angular/core';
 import { addMonths, set } from 'date-fns';
 import { ModalController } from '@ionic/angular';
 import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
-import { Expense } from '../../shared/domain';
+import { Expense, ExpenseCriteria } from '../../shared/domain';
+import { FormBuilder } from '@angular/forms';
+import { CategoryService } from '../../category/category.service';
+import { ToastService } from '../../shared/service/toast.service';
+import { ExpenseService } from '../expense.service';
+import { finalize, from, groupBy, mergeMap, toArray } from 'rxjs';
+import { formatPeriod } from '../../shared/period';
+
+interface ExpenseGroup {
+  date: string;
+  expenses: Expense[];
+}
 
 @Component({
   selector: 'app-expense-overview',
@@ -10,11 +21,68 @@ import { Expense } from '../../shared/domain';
 })
 export class ExpenseListComponent {
   date = set(new Date(), { date: 1 });
+  expenseGroups: ExpenseGroup[] | null = null;
+  readonly initialSort = 'date,desc';
+  lastPageReached = false;
   loading = false;
+  searchCriteria: ExpenseCriteria = { page: 0, size: 25, sort: this.initialSort };
 
-  constructor(private readonly modalCtrl: ModalController) {}
+  constructor(
+    private readonly modalCtrl: ModalController,
+    private readonly expenseService: ExpenseService,
+    private readonly toastService: ToastService,
+    private readonly categoryService: CategoryService,
+    private readonly formBuilder: FormBuilder,
+  ) {}
 
-  //Methode zum
+  //Methode zum initialen Laden der Ausgaben
+  ionViewDidEnter(): void {
+    this.loadExpenses();
+  }
+
+  //Methode zum Laden der Ausgaben
+  private loadExpenses(next: () => void = () => {}): void {
+    this.searchCriteria.yearMonth = formatPeriod(this.date);
+    if (!this.searchCriteria.categoryIds?.length) delete this.searchCriteria.categoryIds;
+    if (!this.searchCriteria.name) delete this.searchCriteria.name;
+    this.loading = true;
+    const groupByDate = this.searchCriteria.sort.startsWith('date');
+    this.expenseService
+      .getExpenses(this.searchCriteria)
+      .pipe(
+        finalize(() => (this.loading = false)),
+        mergeMap((expensePage) => {
+          this.lastPageReached = expensePage.last;
+          next();
+          if (this.searchCriteria.page === 0 || !this.expenseGroups) this.expenseGroups = [];
+          return from(expensePage.content).pipe(
+            groupBy((expense) => (groupByDate ? expense.date : expense.id)),
+            mergeMap((group) => group.pipe(toArray())),
+          );
+        }),
+      )
+      .subscribe({
+        next: (expenses: Expense[]) => {
+          const expenseGroup: ExpenseGroup = {
+            date: expenses[0].date,
+            expenses: this.sortExpenses(expenses),
+          };
+          const expenseGroupWithSameDate = this.expenseGroups!.find((other) => other.date === expenseGroup.date);
+          if (!expenseGroupWithSameDate || !groupByDate) this.expenseGroups!.push(expenseGroup);
+          else
+            expenseGroupWithSameDate.expenses = this.sortExpenses([
+              ...expenseGroupWithSameDate.expenses,
+              ...expenseGroup.expenses,
+            ]);
+        },
+        error: (error) => this.toastService.displayErrorToast('Could not load expenses', error),
+      });
+  }
+
+  //Methode zum Sortieren der Ausgaben
+  private sortExpenses = (expenses: Expense[]): Expense[] => expenses.sort((a, b) => a.name.localeCompare(b.name));
+
+  //Methode um Monate hinzuzufügen
   addMonths = (number: number): void => {
     this.date = addMonths(this.date, number);
   };
